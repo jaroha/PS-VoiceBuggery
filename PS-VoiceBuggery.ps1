d1312d87f8fe0ecf4e97ffb5e3f7fde701f9a2b0
James <jaroha@me.com>
2019-10-04 12:26:29 +1000

    Initial Commit

---
+++ PS-VoiceBuggery.ps1
@@ -0,0 +1,31 @@
+﻿## Load Speech Object
+[Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null
+## Load Sound Control
+$volume = new-object -com wscript.shell
+## Specify path to text file after '$phrasefile ='
+## Text file should be formated by Seperating each phrase like so:
+## "Phrase 1",
+## "Phrase 2",
+## "Phrase 3",
+$phrasefile = "D:\text.txt"
+## Just some error handling, if the file does not exist, spit out the random phrases below (they're stupid lyrics I found on a website)
+if((Test-Path $phrasefile) -eq 0)
+    { $phraselist = "I'm a little teapot short an stout, here is my handle, here is my spout","Me not working hard? Yea, right! Picture that with a Kodak And, better yet, go to Times Square Take a picture of me with a Kodak.”,“Only time will tell if we stand the test of time.” }
+elseif ((Test-Path $phrasefile) -eq 1)
+    { $phraselist = Get-Content $phrasefile }
+#pull random phrase from either the file (if it exists) or the random phrases
+$phrase = Get-Random $phraselist
+## set the random speed
+$ratenum = Get-Random -Maximum 4 -Minimum -5
+$object = New-Object System.Speech.Synthesis.SpeechSynthesizer
+## default speech voices installed in windows, additional ones can be downloaded from MS --
+## leaving this in comment 
+$voices = "Microsoft Hazel Desktop","Microsoft Zira Desktop","Microsoft David Desktop"
+## Set Volume to max, no matter what
+1..100 | % {$volume.SendKeys([char]175)}
+##select the random voice
+$voice = Get-Random $voices
+$object.SelectVoice($voice)
+$object.Rate = $ratenum
+## say it
+$object.Speak($phrase)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PS-VoiceBuggery
+Just for fun.
+
+Run it as a logon script, or a random scheduled task. It'll pull from a list of phrases or some random things I added, just to make things extra annoying, it unmutes the speaker and sets the volume to max.
